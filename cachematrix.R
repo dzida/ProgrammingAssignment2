22de0d7b35d681fa7748811d76b0ae123f2548b6
Lukasz Dziedzia <lukasz@connectv.com>
2015-05-22 21:33:03 +0200

    better comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Below functions allow to avoid potentially expensive matrix iverse computing,
-## by caching inverse computed once for a given instance of matrix
+## by caching inverse computed for a given instance of a matrix.
 ## example usage:
 ## m <- matrix(1:4, 2, 2)
 ## cm <- makeCacheMatrix(m)
@@ -10,23 +10,27 @@
 # Creates a structure that holds matrix data (x)
 # and cached matrix's inverse (inv).
 # Returs list of function references that allow
-# to get and set matric data and inverse for this matrix.
+# to get and set a matrix data and an inverse for this matrix.
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL  # inverse cache
     set <- function(y) {
       x <<- y
-      inv <<- NULL
+      inv <<- NULL  # reset cached inverse
     }
     get <- function() x
     set_inverse <- function(inverse) inv <<- inverse
     get_inverse <- function() inv
+    
+    # return list of operations
     list(get=get, set=set, get_inverse=get_inverse, set_inverse=set_inverse)
 }
 
 
 # Returns inverse for a matrix.
-# Gets an inverse from cacfe if exists. Otherwise populates cache with computed matrix inverse.
-# Works only with objects returned from makeCacheMatrix
+# Gets an inverse from cache if exists,
+# otherwise populates cache with computed matrix inverse before returning this value.
+# Works only with objects returned from makeCacheMatrix.
+# Provided matrix is assumed to be invertible.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     
@@ -34,7 +38,7 @@ cacheSolve <- function(x, ...) {
     inv <- x$get_inverse()
     
     if(!is.null(inv)){
-        # if there is a cached inverse, use it
+        # if there is a cached inverse, return it
         return(inv)         
     }
     
@@ -44,5 +48,7 @@ cacheSolve <- function(x, ...) {
     
     # populate cache
     x$set_inverse(inv)
+    
+    # return computed inverse
     inv
 }
